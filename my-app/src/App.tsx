343ece497f8b65729552c3a4f871470de02a196f
Abdelrahman Elgendy <abdelelgendy11@gmail.com>
2026-02-01 17:50:39 -0500

    feat: application list with state and add action

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,51 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Header from './Components/Header'
+
+interface Application {
+  id: number
+  company: string
+  role: string
+}
+
+
+
 function App() {
+  const [applications, setApplications] = useState<Application[]>([])
+  
+useEffect(() => {
+  setApplications([
+    { id: 1, company: "Google", role: "Software Intern" },
+    { id: 2, company: "Meta", role: "Frontend Intern" }
+  ])
+}, [])
+
+function addApplication() {
+  setApplications([
+    ...applications,
+    {
+      id: Date.now(),
+      company: "New Company",
+      role: "New Role"
+    }
+  ])
+}
+
   return (
     <div>
       <Header title='Internship tracker'/>
+
+      <button onClick={addApplication}>
+        Add Application 
+      </button>
+
+      <ul>
+        {applications.map(app => (
+          <li key={app.id}>
+            {app.company} - {app.role}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
