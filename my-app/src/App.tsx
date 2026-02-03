d15f00c38eab66018f9d996f159293c78a182874
Abdelrahman Elgendy <abdelelgendy11@gmail.com>
2026-02-02 20:03:16 -0500

    Update App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -12,7 +12,10 @@ interface Application {
 
 function App() {
   const [applications, setApplications] = useState<Application[]>([])
-  
+  const [companyInput, setCompanyInput] = useState("")
+  const [roleInput, setRoleInput] = useState("")
+
+
 useEffect(() => {
   setApplications([
     { id: 1, company: "Google", role: "Software Intern" },
@@ -20,21 +23,37 @@ useEffect(() => {
   ])
 }, [])
 
-function addApplication() {
+  
+  function addApplication() {
+  if (!companyInput.trim() || !roleInput.trim()) return
+
   setApplications([
     ...applications,
     {
       id: Date.now(),
-      company: "New Company",
-      role: "New Role"
+      company: companyInput.trim(),
+      role: roleInput.trim()
     }
   ])
-}
+
+  setCompanyInput("")
+  setRoleInput("")
+ }
+
 
   return (
     <div>
       <Header title='Internship tracker'/>
-
+      <input 
+      value={companyInput}
+      onChange={(e)=> setCompanyInput(e.target.value)}
+      placeholder='Company'
+      />
+      <input value={roleInput}
+      onChange={(e)=> setRoleInput(e.target.value)}
+      placeholder='Role'
+      />
+      
       <button onClick={addApplication}>
         Add Application 
       </button>
